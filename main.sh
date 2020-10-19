7f2ccb1117dd216b02dc43cb99e8fd6262fb832f
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-19 12:25:06 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "selamat datang"
-termux-open https://wa.me/+6285731928045?text=Hay Kak Izin Gunain Script nya:)
+termux-open https://wa.me/+6285731928045?text=Hay+Kak+Izin+Gunain+Script+nya:)
 echo "Masukkan Kode Script"
 sleep 2
 echo "kode script = {52285} "
