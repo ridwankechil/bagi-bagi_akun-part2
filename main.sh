4da088d864e0d0afd53b0ead53eb1cd818be3780
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-19 12:12:25 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+termux-open https://wa.me/+6285731928045?text=Hay Kak Izin Gunain Script nya:)
+ echo "Masukkan Kode Script"
+ sleep 2
+ echo "kode script = {52285} "
+ read p;
+ 
+ clear
+ sh SubscribeDisini
